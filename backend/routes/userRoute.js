aba24bd6f28e4a4ca18e30c4e7bc716fcc6c6f63
emanmacario <emacario1997@hotmail.com>
2020-07-05 22:43:44 +1000

    Add require for passport

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 let User = require('../models/userModel');
 
 router.route('/').get((req, res) => {
