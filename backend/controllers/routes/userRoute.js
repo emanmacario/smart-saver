6572f90bf56c4de1dcac0cd697449fbdfa1db582
emanmacario <emacario1997@hotmail.com>
2020-10-19 16:45:12 +1100

    Move user registration to user service

--- backend/controllers/routes/userRoute.js
+++ backend/controllers/routes/userRoute.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const passport = require('passport');
 const mongoose = require('mongoose');
+const isAuth = require('../middlewares/authMiddleware');
 const User = require('../../models/userModel');
+const UserService = require('../../services/userService');
 
 // Suppress Mongoose deprecation warnings
 mongoose.set('useFindAndModify', false);
@@ -17,37 +19,45 @@ router.route('/').get((req, res) => {
 });
 
 // User sign up endpoint
-router.route('/register').post((req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
-
-  const newUser = new User({
+router.route('/register').post(async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = {
     username: username,
     email: email,
     password: password,
     products: []
-  });
+  }
 
-  // Check if username is available
-  User.findOne({ username: username })
-    .then((user) => {
-      // User with username already exists
-      if (user) {
-        return res.status(400).json({ success: false, message: 'Sorry, that username has already been taken' });
-      }
-      // Username is available, save new user
-      newUser.save()
-      .then(() => {
-        res.status(200).json({ success: true, message: 'Your account has successfully been created' });
-      })
-      .catch((err) => {
-        res.status(500).json({ success: false, message: 'Sorry, we could not create your account right now. Please try again!' });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+  const { code, success, message } = await UserService.register(user);
+  res.status(code).json({ success: success, message: message });
+
+
+  // const newUser = new User({
+  //   username: username,
+  //   email: email,
+  //   password: password,
+  //   products: []
+  // });
+
+  // // Check if username is available
+  // User.findOne({ username: username })
+  //   .then((user) => {
+  //     // User with username already exists
+  //     if (user) {
+  //       return res.status(400).json({ success: false, message: 'Sorry, that username has already been taken' });
+  //     }
+  //     // Username is available, save new user
+  //     newUser.save()
+  //     .then(() => {
+  //       res.status(200).json({ success: true, message: 'Your account has successfully been created' });
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({ success: false, message: 'Sorry, we could not create your account right now. Please try again!' });
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
 });
 
 // User login endpoint
@@ -73,13 +83,9 @@ router.post('/login', (req, res, next) => {
 });
 
 // User logout endpoint
-router.route('/logout').get((req, res) => {
-  if (req.user) {
-    req.logout();
-    res.json({ message: "Logged user out" });
-  } else {
-    res.json({ message: "No user to log out" });
-  }
+router.route('/logout').get(isAuth, (req, res) => {
+  req.logout();
+  res.json({ message: "User logged out successfully" });
 });
 
 
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,2 +1,39 @@
 const { Product } = require('../models/productModel');
 const User = require('../models/userModel');
+
+class UserService {
+
+  /**
+   * Registers a user in the database (if their given username 
+   * has not already been taken)
+   * @param {object} user user object containing username, email, and password
+   */
+  static async register(user) {
+    // Create a new user
+    let newUser = User(user);
+
+    // Check for an existing user with the same username
+    try {
+      const { username } = user;
+      let existingUser = await User.findOne({ username: username });
+      if (existingUser) {
+        return { code: 400, success: false, message: 'Sorry, that username has already been taken' }
+      }
+    } catch (err) {
+      console.warn(err);
+      return { code: 500, success: false, message: 'Sorry, we could not create your account right now' }
+    }
+    
+
+    // User credentials are valid, save user in the database
+    try {
+      await newUser.save();
+      return { code: 200, success: true, message: 'Your account has been created' }
+    } catch (err) {
+      console.warn(err)
+      return { code: 500, success: false, message: 'Sorry, we could not create your account right now' }
+    }
+  }
+}
+
+module.exports = UserService;
